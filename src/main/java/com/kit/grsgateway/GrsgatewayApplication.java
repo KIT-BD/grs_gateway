1ff3f774fabbc706e5f5789dff2053ba250ddf0b
anwar336 <anwar336@gmail.com>
2024-12-17 09:58:22 +0600

    Initial commit

---
+++ src/main/java/com/kit/grsgateway/GrsgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kit.grsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrsgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrsgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kit/grsgateway/GrsgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.grsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrsgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
