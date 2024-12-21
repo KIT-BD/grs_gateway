d5623df0f4b34e388054667ccdd0be621cd95c12
anwar336 <anwar336@gmail.com>
2024-12-21 13:57:15 +0600

    update

---
+++ src/main/java/com/kit/grsgateway/filter/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.kit.grsgateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+//@Configuration
+public class CorsConfig {
+    //@Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true); // Allow credentials like cookies
+        corsConfiguration.addAllowedOriginPattern("*"); // Allow all origins
+        corsConfiguration.addAllowedHeader("*"); // Allow all headers
+        corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/kit/grsgateway/filter/LoggingFilter.java
+++ src/main/java/com/kit/grsgateway/filter/LoggingFilter.java
@@ -1,17 +1,23 @@
 package com.kit.grsgateway.filter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Component
+@Slf4j
 public class LoggingFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("Request: " + exchange.getRequest().getPath());
+        UUID uuid = UUID.randomUUID();
+        String cid = uuid.toString();
+        log.info("[{}] Request: {} ", cid, exchange.getRequest().getPath());
         return chain.filter(exchange)
-                .then(Mono.fromRunnable(() -> System.out.println("Response: " + exchange.getResponse().getStatusCode())));
+                .then(Mono.fromRunnable(() -> log.info("[{}] Response: {} : {} ",cid, exchange.getRequest().getPath(), exchange.getResponse().getStatusCode())));
     }
 }
